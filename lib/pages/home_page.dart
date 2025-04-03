7cfb63d016e68a08f76cbd70bf890b5bbd827d24
Toretay <abdymvzhit@gmail.com>
2025-04-03 16:09:48 +0500

    added appbar with exit from profile button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () async {
+            await authService.value.signOut();
+            if (context.mounted) Navigator.pop(context);
+          },
+          icon: Icon(Icons.exit_to_app),
+        ),
+      ),
+    );
   }
 }
