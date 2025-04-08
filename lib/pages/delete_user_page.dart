874c481f1f5aa2fb9a838ede8992a93c10913d6f
Toretay <abdymvzhit@gmail.com>
2025-04-08 15:07:32 +0500

    created textfields with text controllers

--- lib/pages/delete_user_page.dart
+++ lib/pages/delete_user_page.dart
@@ -8,10 +8,36 @@ class DeleteUserPage extends StatefulWidget {
 }
 
 class _DeleteUserPageState extends State<DeleteUserPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Delete user"), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                hintText: "Your email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                hintText: "Your password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
