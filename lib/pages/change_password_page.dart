1415cf671d70a3943d8718100aaee5f64bcc518f
Toretay <abdymvzhit@gmail.com>
2025-04-08 14:50:34 +0500

    added padding to column

--- lib/pages/change_password_page.dart
+++ lib/pages/change_password_page.dart
@@ -18,34 +18,37 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
     return Scaffold(
       appBar: AppBar(title: Text("Change password"), centerTitle: true),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                hintText: "Your email",
-                border: OutlineInputBorder(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  hintText: "Your email",
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            SizedBox(height: 12.0),
-            TextField(
-              controller: _currentPasswordController,
-              decoration: InputDecoration(
-                hintText: "Your current password",
-                border: OutlineInputBorder(),
+              SizedBox(height: 12.0),
+              TextField(
+                controller: _currentPasswordController,
+                decoration: InputDecoration(
+                  hintText: "Your current password",
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            SizedBox(height: 12.0),
-            TextField(
-              controller: _newPasswordController,
-              decoration: InputDecoration(
-                hintText: "Your new password",
-                border: OutlineInputBorder(),
+              SizedBox(height: 12.0),
+              TextField(
+                controller: _newPasswordController,
+                decoration: InputDecoration(
+                  hintText: "Your new password",
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
